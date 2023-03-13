a8418ae107174e1b2a5d70c87d68a1a02da44efb
Jhonatas <jhonatasmoraes@alunos.utfpr.edu.br>
2023-03-12 21:58:28 -0300

    Adicionados os menus e itens bitmaps

--- VAS_Programa/src/main.cpp
+++ VAS_Programa/src/main.cpp
@@ -1,270 +1,93 @@
 #include <Arduino.h>
+#include <U8g2lib.h>
 #include <Wire.h>
-#include <SPI.h>
-//#include <MechaQMC5883.h>
 #include <TinyGPSPlus.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-//#include <Adafruit_HMC5883_U.h>
-//#include <Adafruit_Sensor.h>
 #include <QMC5883LCompass.h>
+#include <telas_menu.h>
+#include <math.h>
+#include <EEPROM.h>
+#include <ESP32Servo.h>
 
 QMC5883LCompass compass;
+U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+Servo ESCA; //motor direito
+Servo ESCB; //motor esquerdo
 
-/*
-//Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+int ESCA_Pin = 18;
+int ESCB_Pin = 19;
 
-//O calculo do desvio magnético pode ser calculado pelos dados encontrados no site https://www.ngdc.noaa.gov/geomag/calculators/magcalc.shtml
-#define mag_dev_g 19 //Desvio magnético calculado na cidade de blumenau de 19° 51' W -- Pelo mapa obtido no WMM, o desvio é negativo
-#define mag_dev_m 51 //Se é W deve subtrair do heading
-#define mag_dev_p 'W'
-double desvio;
-
-
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-//MechaQMC5883 bussola; //Criacao do objeto para o sensor 
-TinyGPSPlus gps;
-
-int x = 0, y = 0, z = 0, a = 0;
-double angulo = 0;
+int minUs = 1000; //tempo de pulso menor
+int maxUs = 2000; //tempo de pulso maior
+int ESCA_Vel = 0; //velocidade inicial
+int ESCB_Vel = 0;
 
 
-double gms_graus(double graus, double minutos, char direcao){
-  int sinal = 1;
-  if(direcao == 'W'){
-    sinal = -1;
-  }else if(direcao == 'E'){
-    sinal = 1;
-  }
-  return sinal * (graus + (minutos/60)); //após este passo, deve-se somar com o valor medido este obtido
-}
+#define EEPROM_SIZE 8
 
-void PreparaGPS(){
+//Variaveis Globais
+int selectedMenuItem = 0;
+int numMenuItems = 3;
+int NivelMenu = 0;
+int SelecN1 = 0;
+int SelecN2 = 0;
+int btn_1 = 12; //Botao desce32
+int btn_2 = 13; //Botao sobe33
+int btn_3 = 14; //Botao entra34
+int16_t max_x = -32768;
+int16_t min_x = 32767;
+int16_t max_y = -32768;
+int16_t min_y = 32767;
+int16_t x, y; //, z;
 
-}
+const int NUM_ITENS = 6;
 
-void PreparaMagn(){
-compass.init();
-compass.setSmoothing(10,true);  
+char menu_items[NUM_ITENS] [20] = {
+  {"Cal. QMC5883"},
+  {"GPS"},
+  {"Outro"},
+  {"Iniciar"},
+  {"Ler Dados"},
+  {"Motores"}
+};
 
+int item_sel = 1; //Qual item do menu está selecionado
+int item_ant; //Item anterior ao selecionado
+int item_pos; //Item posterior ao selecionado
 
-}
+int btn_1_clic=0;
+int btn_2_clic=0;
+int btn_3_clic=0;
 
-void PreparaDisplay(){
-   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)){ 
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-  display.display();
-  delay(2000); // Pause for 2 seconds
-  // Clear the buffer
-  display.clearDisplay();
-  display.setTextSize(1);         // set text size
-  display.setTextColor(WHITE);    // set text color
-  display.setCursor(0, 0);       // set position to display
-}
+int TelaAtual=0;
 
+float direc = 0;
 
-void displayInfo(){
-  Serial.print(F("Location: "));
-
-  if (gps.location.isValid()){
-    Serial.print("Lat: ");
-    Serial.print(gps.location.lat(), 6);
-    Serial.print(F(","));
-    Serial.print("Lng: ");
-    Serial.print(gps.location.lng(), 6);
-    Serial.println();
-  }else{
-    Serial.println(F("INVALID"));
-  }
-}
-
-
-void calibration(){
-
-compass.init();
-int calibrationData[3][2];
-bool changed = false;
-bool done = false;
-int t = 0;
-int c = 0;
-Serial.println("This will provide calibration settings for your QMC5883L chip. When prompted, move the magnetometer in all directions until the calibration is complete.");
-Serial.println("Calibration will begin in 5 seconds.");
-delay(5000);
-while(done == false){
-  int x, y, z;
-  
-  // Read compass values
-  compass.read();
-
-  // Return XYZ readings
-  x = compass.getX();
-  y = compass.getY();
-  z = compass.getZ();
-
-  changed = false;
-
-  if(x < calibrationData[0][0]) {
-    calibrationData[0][0] = x;
-    changed = true;
-  }
-  if(x > calibrationData[0][1]) {
-    calibrationData[0][1] = x;
-    changed = true;
-  }
-
-  if(y < calibrationData[1][0]) {
-    calibrationData[1][0] = y;
-    changed = true;
-  }
-  if(y > calibrationData[1][1]) {
-    calibrationData[1][1] = y;
-    changed = true;
-  }
-
-  if(z < calibrationData[2][0]) {
-    calibrationData[2][0] = z;
-    changed = true;
-  }
-  if(z > calibrationData[2][1]) {
-    calibrationData[2][1] = z;
-    changed = true;
-  }
-
-  if (changed && !done) {
-    Serial.println("CALIBRATING... Keep moving your sensor around.");
-    c = millis();
-  }
-    t = millis();
-  
-  
-  if((t - c > 5000) && !done) {
-    done = true;
-    Serial.println("DONE. Copy the line below and paste it into your projects sketch.);");
-    Serial.println();
-      
-    Serial.print("compass.setCalibration(");
-    Serial.print(calibrationData[0][0]);
-    Serial.print(", ");
-    Serial.print(calibrationData[0][1]);
-    Serial.print(", ");
-    Serial.print(calibrationData[1][0]);
-    Serial.print(", ");
-    Serial.print(calibrationData[1][1]);
-    Serial.print(", ");
-    Serial.print(calibrationData[2][0]);
-    Serial.print(", ");
-    Serial.print(calibrationData[2][1]);
-    Serial.println(");");
-    compass.setCalibration(calibrationData[0][0], calibrationData[0][1], calibrationData[1][0], calibrationData[1][1], calibrationData[2][0], calibrationData[2][1]);
-    }
-}
-}
-
-
-void setup() {
-  desvio = gms_graus(mag_dev_g, mag_dev_m, mag_dev_p); //A medida real é o polo magnético menos o desvio
-  Serial.begin(9600);
-  Serial2.begin(9600);
-  Serial.println(desvio);
-  Wire.begin();
-  PreparaGPS();
-  PreparaMagn();
-  PreparaDisplay();
-  bool calibrar = true;
-  if (!calibrar){
-    calibration();
-  }else{
-    PreparaMagn();
-  }
-}
-
-void loop() {
-
-//  while (Serial2.available() > 0)
-//    if(gps.encode(Serial2.read()))
-//      displayInfo(); //Mostrar as informações quando tiver
-//  if (millis() > 5000 && gps.charsProcessed() < 10)
-//  {
-//    Serial.println(F("No GPS detected: check wiring."));
-//    while (true);
-//  }
-
- 
-// Display the results (magnetic vector values are in micro-Tesla (uT)) 
-//Serial.print("X: ");
-//Serial.print(event.magnetic.x);
-//Serial.print(" ");
-//Serial.print("Y: ");
-//Serial.print(event.magnetic.y);
-//Serial.print(" ");
-//Serial.print("Z: ");
-//Serial.print(event.magnetic.z);
-//Serial.print(" ");
-//Serial.println("uT");
-  compass.read();
-  byte a = compass.getAzimuth();
-
-  char myArray[3];
-  compass.getDirection(myArray, a);
-  
-  Serial.print(myArray[0]);
-  Serial.print(myArray[1]);
-  Serial.print(myArray[2]);
-  Serial.println();
-  
-  // Return XYZ readings
-  x = compass.getX();
-  y = compass.getY();
-  z = compass.getZ();
-  //bussola.read(&x,&y,&z,&a); //Obter o valor dos eixos X, Y e Z do Sensor
-  angulo = atan2(x, y)/0.0174532925; //Calculo do angulo usando os eixos X e Y atraves da formula = atan2(x, y)*180/pi
-  angulo = angulo - desvio;
-
-  
-  //Ajuste do angulo entre 0 e 360 graus
-  if(angulo < 0) {
-  angulo+=360;}
-  
-  angulo = 360-angulo;
-  Serial.println(angulo); //Imprime o valor do angulo na Serial do Arduino
-  Serial.println("x = "+(String)x+" , y = "+(String)y+" , z = "+(String)z+" , a = "+(String)a);
-
-  delay(1000); //Delay de 500 ms entre novas leituras
-
-  // put your main code here, to run repeatedly:
-}
-
-*/
-
-
-
+float declinacao = 19.85;
 
 
 
 void calibrar_qmc5883(){
+  ////////u8g2.clearDisplay();
+  //u8g2.setCursor(0,0);
+  //u8g2.println("Calibracao em 5s");
+  //u8g2.println();
+  //Serial.println("A calibrção começa em 5 segundos, movimente o dispositivo");
+  compass.setCalibration(-32768, 32767, -32768, 32767, -32768, 32767);
 
-  Serial.println("A calibrção começa em 5 segundos, movimente o dispositivo");
-  delay(5000);
+  //delay(5000);
 
   //int t_atual = millis(); //marca o instante de inicio agora
   int tempo_final = millis() + 5000; //marca o instante de final da calibração
 
-  int max_x = -32768;
-  int min_x = 32767;
-  int max_y = -32768;
-  int min_y = 32767;
+  max_x = -32768;
+  min_x = 32767;
+  max_y = -32768;
+  min_y = 32767;
   //int max_z = -32768;
   //int min_z = 32767;
   bool novo_v = false;
   bool pronto = false;
-  int x, y; //, z;
+  x = y = 0; //, z;
 
   while(!pronto){
     compass.read();
@@ -272,62 +95,231 @@ void calibrar_qmc5883(){
     y = compass.getY();
     //z = compass.getZ();
     novo_v = false;
-    if (x > max_x) max_x = x; novo_v = true;
-    if (x < min_x) min_x = x; novo_v = true;
-    if (y > max_y) max_y = y; novo_v = true;
-    if (y < min_y) min_y = y; novo_v = true;
+    if (x > max_x){max_x = x; novo_v = true;}
+    if (x < min_x){min_x = x; novo_v = true;}
+    if (y > max_y){max_y = y; novo_v = true;}
+    if (y < min_y){min_y = y; novo_v = true;}
     //if (z > max_z) max_z = z; novo_v = true;
     //if (z < min_z) min_z = z; novo_v = true;
-    if (novo_v && !pronto) tempo_final = millis() + 5000; Serial.println("Calibrando . . .");
-    if(millis() > tempo_final) pronto = true; compass.setCalibration(min_x, max_x, min_y, max_y, -32768, 32767);
+    if (novo_v == true && pronto == false){ 
+      tempo_final = millis() + 5000; 
+      Serial.println("Calibrando . . ."); //u8g2.setCursor(0,9); u8g2.println("Calibrando...");  u8g2.println(); u8g2.display();
+      novo_v = false;
+      }
+
+    if (millis() > tempo_final){
+      pronto = true; 
+      compass.setCalibration(min_x, max_x, min_y, max_y, -32768, 32767); 
+      EEPROM.put(0, min_x >> 8);
+      EEPROM.put(1, min_x & 0x00FF);
+      EEPROM.put(2, max_x >> 8);
+      EEPROM.put(3, max_x & 0x00FF);
+      EEPROM.put(4, min_y >> 8);
+      EEPROM.put(5, min_y & 0x00FF);
+      EEPROM.put(6, max_y >> 8);
+      EEPROM.put(7, max_y & 0x00FF);
+
+      EEPROM.commit();
+      Serial.println("Pronto!");//u8g2.setCursor(0,9); u8g2.println("Pronto!");  u8g2.println(); u8g2.display();
+      }
   }
 }
 
-int btn_1 = 32;
-int btn_2 = 33;
-int btn_3 = 34;
+void MostraTela(){
+u8g2.setBitmapMode(1); //faz os bitmaps transparentes
 
+switch (TelaAtual)
+{
+case 0:
 
-void setup() {
-  Serial.begin(9600);
-  Wire.begin();
-  compass.init();
-  pinMode(btn_1,INPUT_PULLUP);
-  pinMode(btn_2,INPUT_PULLUP);
-  pinMode(btn_3,INPUT_PULLUP);
+  u8g2.firstPage();
+  do{
+    //Item Anterior  
+    u8g2.setFont(u8g2_font_7x14_tf);
+    u8g2.drawStr(26,15,menu_items[item_ant]);
+    u8g2.drawBitmap(4,2,16/8,16, Bitmap_Icons[item_ant]); //Desenha Icone Pos(0)
+    //Item Selecionado
+    u8g2.setFont(u8g2_font_7x14B_tf);
+    u8g2.drawStr(26,37,menu_items[item_sel]);
+    u8g2.drawBitmap(4,24,16/8,16, Bitmap_Icons[item_sel]); //Desenha Icone GPS Pos(1)
+    //Item Posterior
+    u8g2.setFont(u8g2_font_7x14_tf);
+    u8g2.drawStr(26,59,menu_items[item_pos]);
+    u8g2.drawBitmap(4,46,16/8,16, Bitmap_Icons[item_pos]); //Desenha Iconeo Pos(2)
+
+    u8g2.drawBitmap(0,22,128/8,21, epd_bitmap_BordaSelec); //Desenha selecionado
+    //u8g2.drawBitmap(120,0,8/8,64, epd_bitmap_BarraRolagem); //Desenha Barra de rolagem
+  }while(u8g2.nextPage());
+  break;
+
+case 1:
+  u8g2.firstPage();
+  do{
+    u8g2.setFont(u8g2_font_7x14_tf);
+    u8g2.drawStr(4,15,"A calibrar em");//cabe 12
+    u8g2.drawStr(4,37,"5 segundos");//cabe 12
+  }while(u8g2.nextPage());
+  delay(5000);
+  u8g2.firstPage();
+  do{
+    u8g2.drawStr(4,15,"Calibrando");//cabe 12
+    //u8g2.drawStr(4,15,"5 segundos");//cabe 12
+  }while(u8g2.nextPage());
+  calibrar_qmc5883();
+  u8g2.firstPage();
+  do{
+    u8g2.drawStr(4,15,"Calibrando");//cabe 12
+    u8g2.drawStr(4,37,"Pronto!");//cabe 12
+  }while(u8g2.nextPage());
+  delay(2000);
+  TelaAtual = 0; //volta para o menu principal
+  break;
+
+case 2:
+      u8g2.firstPage();
+      do{
+        u8g2.drawBitmap(0,21,128/8,21, epd_bitmap_BordaSelec); //Desenha selecionado
+        u8g2.drawBitmap(0,43,128/8,21, epd_bitmap_BordaSelec); //Desenha selecionado
+        u8g2.drawBox(4,23,map(ESCA_Vel, 0, 180, 0,114),16);
+        u8g2.drawBox(4,45,64,16);
+      }while(u8g2.nextPage());
+  break;
+
+case 3:
+    u8g2.firstPage();
+  do{
+    u8g2.setFont(u8g2_font_7x14_tf);
+    u8g2.drawStr(4,15,"Apontando para");//cabe 12
+    
+    u8g2.setCursor(4, 37);
+    u8g2.print(direc);
+  }while(u8g2.nextPage());
+  break;
+  
+default:
+  TelaAtual = 0;
+  break;
+}
 }
 
-void loop() {
-  int x, y, z;
-  //compass.read(&x, &y, &z);
+
+void CalculaGraus(){
+
   compass.read();
   x = compass.getX();
   y = compass.getY();
-  z = compass.getZ();
-
-
-  // Calibrate the readings
-  //int offset_x = 161; // Replace with your own calibration value
-  //int offset_y = -1541; // Replace with your own calibration value
-  //int offset_z = 0; // Replace with your own calibration value
-
-  //x = x - offset_x;
-  //y = y - offset_y;
-  //z = z - offset_z;
-
-  // Calculate the heading
-  float heading = atan2(y, x);
-  if (heading < 0) {
-    heading = 2 * PI + heading;
+  direc = atan2(y,x) * 180 / 3.141592653;
+  direc = direc - declinacao;
+  if (direc < 0) {
+    direc += 360;
   }
-  heading = heading + radians(-19); // Magnetic declination of -19 degrees
-  if (heading < 0) {
-    heading = 2 * PI + heading;
+
+
+}
+
+
+void ControlaESC(){
+
+  ESCA.write(ESCA_Vel);
+  ESCB.write(ESCB_Vel);
+}
+
+ESP32PWM pwm;
+void setup(void) {
+  compass.init();
+  u8g2.begin();
+  Serial.begin(115200);
+  EEPROM.begin(EEPROM_SIZE);
+  min_x=EEPROM.read(0)<<8|EEPROM.read(1);
+  max_x=EEPROM.read(2)<<8|EEPROM.read(3);
+  min_y=EEPROM.read(4)<<8|EEPROM.read(5);
+  max_y=EEPROM.read(6)<<8|EEPROM.read(7);
+  compass.setCalibration(min_x, max_x, min_y, max_y, -32768, 32767); 
+  pinMode(btn_1, INPUT_PULLUP);
+  pinMode(btn_2, INPUT_PULLUP);
+  pinMode(btn_3, INPUT_PULLUP);
+  TelaAtual = 0;
+
+  ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+  ESCA.setPeriodHertz(50);      // Standard 50hz servo
+	ESCB.setPeriodHertz(50);      // Standard 50hz servo
+  ESCA.attach(ESCA_Pin, minUs, maxUs);
+  ESCB.attach(ESCB_Pin, minUs, maxUs);
+
+}
+
+void loop(void) {
+
+
+  if((digitalRead(btn_1) == LOW)&&(btn_1_clic == 0)){
+    if(TelaAtual == 0){
+      item_sel = item_sel-1;
+      if(item_sel<0){item_sel = NUM_ITENS-1;}
+    }else if(TelaAtual == 2){
+      ESCA_Vel-= 10;
+    if(ESCA_Vel<0){ESCA_Vel = 0;}
+    }
+
+    btn_1_clic = 1;
   }
-  float degrees = heading * 180 / PI;
 
-  Serial.print("Heading: ");
-  Serial.println(degrees);
+  if((digitalRead(btn_2) == LOW)&&(btn_2_clic == 0)){
+    if(TelaAtual == 0){
+      item_sel = item_sel+1;
+      if(item_sel>= NUM_ITENS){item_sel = 0;}
+    }else if(TelaAtual == 2){
+      ESCA_Vel += 10;
+      if(ESCA_Vel > 180){ESCA_Vel = 180;}
+    }
+    btn_2_clic = 1;
+  }
 
-  delay(100);
+  if((digitalRead(btn_3) == LOW)&&(btn_3_clic == 0)){
+    
+    if(TelaAtual == 0){
+    switch (item_sel)
+    {
+    case 0:
+      TelaAtual = 1;
+      break;
+    case 1:
+      TelaAtual = 2;
+      break;
+    case 2:
+      TelaAtual = 3;
+      break;
+
+    case 5:
+      TelaAtual = 2;
+      break;
+    
+    default:
+      TelaAtual = 0;
+      break;
+    }      }else{
+    TelaAtual = 0;
+  }
+    btn_3_clic = 1;
+
+  }
+
+if((digitalRead(btn_1) == HIGH)&&(btn_1_clic == 1)){btn_1_clic = 0;}
+if((digitalRead(btn_2) == HIGH)&&(btn_2_clic == 1)){btn_2_clic = 0;}
+if((digitalRead(btn_3) == HIGH)&&(btn_3_clic == 1)){btn_3_clic = 0;}
+
+  item_ant = item_sel-1;
+  if(item_ant<0){item_ant = NUM_ITENS-1;}
+  item_pos = item_sel+1;
+  if(item_pos >= NUM_ITENS){item_pos = 0;}
+
+CalculaGraus();
+MostraTela();
+
+
+
+
+
+
+  //delay(1000);
 }
